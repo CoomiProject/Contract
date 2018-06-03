3a1247ce3ddecd76ba4c94c55fe9bf4cb742ed82
Erichin <erichin@r.recruit.co.jp>
2018-06-03 21:45:41 +0900

    update

--- CoomiToken.sol
+++ CoomiToken.sol
@@ -28,287 +28,112 @@ library SafeMath {
 }
 
 
-interface ERC223 {
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
-    function transfer(address to, uint value, bytes data) public;
-    function allowance(address owner, address spender) public view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+// interface ERC223 {
+//     function balanceOf(address who) external pure returns (uint256);
+//     function allowance(address owner, address spender) external view returns (uint256);
+//     function transfer(address to, uint256 value) external;
+//     function transfer(address to, uint value, bytes data) external;
+//     function transferFrom(address from, address to, uint256 value) external returns (bool);
+//     function approve(address spender, uint256 value) external returns (bool);
+//     event Transfer(address indexed from, address indexed to, uint256 value);
+//     event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+//     event Approval(address indexed owner, address indexed spender, uint256 value);
+// }
 
 
-contract ERC223ReceivingContract { 
+contract ERC223ReceivingContract {
     function tokenFallback(address _from, uint _value, bytes _data) public;
 }
 
 
-contract ERC223Token is ERC223 {
+contract ERC223Token {
     using SafeMath for uint;
-    uint public totalSupply;
-    mapping (address => uint256) internal balances;
-    mapping (address => mapping (address => uint256)) internal allowed;
+    uint256 public totalSupply;
+    mapping(address => uint256) internal balances;
+    mapping(address => mapping(address => uint256)) internal allowed;
 
-    function ERC223Token(uint256 totalSupply) public {
-        this.totalSupply = totalSupply;
+    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function ERC223Token(uint256 _totalSupply) public {
+        totalSupply = _totalSupply;
         balances[msg.sender] = totalSupply;
     }
 
-   function transfer(address _to, uint256 _value) public returns (bool) {
-     require(_to != address(0));
-     require(_value <= balances[msg.sender]);
-     balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
-     balances[_to] = SafeMath.add(balances[_to], _value);
-     Transfer(msg.sender, _to, _value);
-     return true;
-   }
 
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-   }
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0));
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
-
-        balances[_from] = SafeMath.sub(balances[_from], _value);
-        balances[_to] = SafeMath.add(balances[_to], _value);
-        allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
-        Transfer(_from, _to, _value);
-        return true;
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
     }
 
-   function approve(address _spender, uint256 _value) public returns (bool) {
-     allowed[msg.sender][_spender] = _value;
-     Approval(msg.sender, _spender, _value);
-     return true;
-   }
-
-  function allowance(address _owner, address _spender) public view returns (uint256) {
-     return allowed[_owner][_spender];
-   }
-
-   function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-     allowed[msg.sender][_spender] = SafeMath.add(allowed[msg.sender][_spender], _addedValue);
-     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-     return true;
-   }
-
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-     uint oldValue = allowed[msg.sender][_spender];
-     if (_subtractedValue > oldValue) {
-       allowed[msg.sender][_spender] = 0;
-     } else {
-       allowed[msg.sender][_spender] = SafeMath.sub(oldValue, _subtractedValue);
-    }
-     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-     return true;
-   }
-   
-  function transfer(address _to, uint _value, bytes _data) public {
-    require(_value > 0 );
-    if(isContract(_to)) {
-        ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-        receiver.tokenFallback(msg.sender, _value, _data);
-    }
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value, _data);
-    }
-    
-  function isContract(address _addr) private returns (bool is_contract) {
-      uint length;
-      assembly {
-            //retrieve the size of the code on target address, this needs assembly
-            length := extcodesize(_addr)
-      }
-      return (length>0);
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
     }
 
-
-}
-
-/**
- * @title Reference implementation of the ERC223 standard token.
- */
-contract ERC223Token is ERC223Interface {
-    using SafeMath for uint;
-
-    mapping(address => uint) balances; // List of user balances.
-    
-    /**
-     * @dev Transfer the specified amount of tokens to the specified address.
-     *      Invokes the `tokenFallback` function if the recipient is a contract.
-     *      The token transfer fails if the recipient is a contract
-     *      but does not implement the `tokenFallback` function
-     *      or the fallback function to receive funds.
-     *
-     * @param _to    Receiver address.
-     * @param _value Amount of tokens that will be transferred.
-     * @param _data  Transaction metadata.
-     */
     function transfer(address _to, uint _value, bytes _data) public {
-        // Standard function transfer similar to ERC20 transfer with no _data .
-        // Added due to backwards compatibility reasons .
         uint codeLength;
 
         assembly {
-            // Retrieve the size of the code on target address, this needs assembly .
             codeLength := extcodesize(_to)
         }
 
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        if(codeLength>0) {
+        if (codeLength > 0) {
             ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
             receiver.tokenFallback(msg.sender, _value, _data);
         }
         emit Transfer(msg.sender, _to, _value, _data);
     }
-    
-    /**
-     * @dev Transfer the specified amount of tokens to the specified address.
-     *      This function works the same with the previous one
-     *      but doesn't contain `_data` param.
-     *      Added due to backwards compatibility reasons.
-     *
-     * @param _to    Receiver address.
-     * @param _value Amount of tokens that will be transferred.
-     */
+
     function transfer(address _to, uint _value) public {
         uint codeLength;
         bytes memory empty;
 
         assembly {
-            // Retrieve the size of the code on target address, this needs assembly .
             codeLength := extcodesize(_to)
         }
 
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        if(codeLength>0) {
+        if (codeLength > 0) {
             ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
             receiver.tokenFallback(msg.sender, _value, empty);
         }
         emit Transfer(msg.sender, _to, _value, empty);
     }
 
-    /**
-     * @dev Returns balance of the `_owner`.
-     *
-     * @param _owner   The address whose balance will be returned.
-     * @return balance Balance of the `_owner`.
-     */
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
-        return balances[_owner];
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        bytes memory empty;
+
+        require(_to != address(0x0));
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        emit Transfer(msg.sender, _to, _value, empty);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
     }
 }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
+contract CoomiToken is ERC223Token {
+    string public constant name = 'Coomi';
+    string public constant symbol = 'COOMI';
+    // same as ether. (1ether=1wei * (10 ** 18))
+    uint public constant decimals = 18;
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() public {
-    owner = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    if (newOwner != address(0)) {
-      owner = newOwner;
-    }
-  }
-}
-
-/**
- * @title Burnable Token
- * @dev Token that can be irreversibly burned (destroyed).
- */
-contract BurnableToken is ERC223Token {
     event Burn(address indexed burner, uint indexed value);
 
-    /**
-     * @dev Burns a specific amount of tokens.
-     * @param _value The amount of token to be burned.
-     */
     function burn(uint _value) public {
         require(_value > 0);
-
         address burner = msg.sender;
         balances[burner] = balances[burner].sub(_value);
         totalSupply = totalSupply.sub(_value);
         emit Burn(burner, _value);
     }
-}
-
-/**
- * @title Mintable token
- * @dev Simple ERC20 Token example, with mintable token creation
- * @dev Issue: * https://github.com/OpenZeppelin/zeppelin-solidity/issues/120
- * Based on code by TokenMarketNet: https://github.com/TokenMarketNet/ico/blob/master/contracts/MintableToken.sol
- */
-contract MintableToken is ERC223Token, Ownable {
-  event Mint(address indexed to, uint256 amount);
-  event MintFinished();
-
-  bool public mintingFinished = false;
-
-  modifier canMint() {
-    require(!mintingFinished);
-    _;
-  }
-
-  /**
-   * @dev Function to mint tokens
-   * @param _to The address that will recieve the minted tokens.
-   * @param _amount The amount of tokens to mint.
-   * @return A boolean that indicates if the operation was successful.
-   */
-  function mint(address _to, uint256 _amount) public onlyOwner canMint returns (bool) {
-    totalSupply = totalSupply.add(_amount);
-    balances[_to] = balances[_to].add(_amount);
-    emit Mint(_to, _amount);
-    return true;
-  }
-
-  /**
-   * @dev Function to stop minting new tokens.
-   * @return True if the operation was successful.
-   */
-  function finishMinting() public onlyOwner returns (bool) {
-    mintingFinished = true;
-    emit MintFinished();
-    return true;
-  }
-}
-
-contract CoomiToken is MintableToken, BurnableToken {
-  string public constant name = 'Coomi';
-  string public constant symbol = 'COOMI';
-  // same as ether. (1ether=1wei * (10 ** 18))
-  uint public constant decimals = 18;
 }
