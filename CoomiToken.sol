1867448fdb3949b622f687103c23c901d6c50794
Erichin <erichin@r.recruit.co.jp>
2018-06-03 22:00:18 +0900

    update

--- CoomiToken.sol
+++ CoomiToken.sol
@@ -60,29 +60,19 @@ contract ERC223Token {
         balances[msg.sender] = totalSupply;
     }
 
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
 
     function allowance(address _owner, address _spender) public view returns (uint256) {
         return allowed[_owner][_spender];
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function transfer(address _to, uint _value, bytes _data) public {
-        uint codeLength;
-
-        assembly {
-            codeLength := extcodesize(_to)
-        }
-
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        if (codeLength > 0) {
-            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-            receiver.tokenFallback(msg.sender, _value, _data);
-        }
-        emit Transfer(msg.sender, _to, _value, _data);
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
     }
 
     function transfer(address _to, uint _value) public {
@@ -102,31 +92,45 @@ contract ERC223Token {
         emit Transfer(msg.sender, _to, _value, empty);
     }
 
+    function transfer(address _to, uint _value, bytes _data) public {
+        uint codeLength;
+
+        assembly {
+            codeLength := extcodesize(_to)
+        }
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        if (codeLength > 0) {
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            receiver.tokenFallback(msg.sender, _value, _data);
+        }
+        emit Transfer(msg.sender, _to, _value, _data);
+    }
+
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        uint codeLength;
         bytes memory empty;
 
-        require(_to != address(0x0));
+        assembly {
+            codeLength := extcodesize(_to)
+        }
+
         allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
         balances[_from] = balances[_from].sub(_value);
         balances[_to] = balances[_to].add(_value);
 
-        emit Transfer(msg.sender, _to, _value, empty);
-        return true;
-    }
+        if (codeLength > 0) {
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            receiver.tokenFallback(msg.sender, _value, empty);
+        }
 
-    function approve(address _spender, uint256 _value) public returns (bool) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
+        emit Transfer(msg.sender, _to, _value, empty);
         return true;
     }
 }
 
-contract CoomiToken is ERC223Token {
-    string public constant name = 'Coomi';
-    string public constant symbol = 'COOMI';
-    // same as ether. (1ether=1wei * (10 ** 18))
-    uint public constant decimals = 18;
-
+contract BurnableERC223Token is ERC223Token {
     event Burn(address indexed burner, uint indexed value);
 
     function burn(uint _value) public {
@@ -136,4 +140,12 @@ contract CoomiToken is ERC223Token {
         totalSupply = totalSupply.sub(_value);
         emit Burn(burner, _value);
     }
+}
+
+contract CoomiToken is BurnableERC223Token {
+    string public constant name = 'Coomi';
+    string public constant symbol = 'COOMI';
+    uint public constant decimals = 18;
+
+    function CoomiToken(uint256 _totalSupply) ERC223Token(_totalSupply) public {}
 }
