863cb1b9bc7c4277d8ad8a442aaac7ea4aa81101
Erichin <erichin@r.recruit.co.jp>
2018-07-21 20:19:32 +0900

    update

--- CoomiToken.sol
+++ CoomiToken.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.4.24;
 import "./SafeMath.sol";
 
 contract ERC20Interface {
-  uint256 public totalSupply;
+  uint256 internal totalSupply;
   mapping(address => uint256) internal balances;
   mapping(address => mapping(address => uint256)) internal allowed;
 
-  function balanceOf(address who) public view returns (uint256);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address owner) public view returns (uint256);
   function allowance(address owner, address spender) public view returns (uint256);
   function approve(address spender, uint256 value) public returns (bool);
   function transfer(address to, uint256 value) public returns (bool);
@@ -22,6 +23,10 @@ contract ERC20Interface {
 contract ERC20Token is ERC20Interface {
   using SafeMath for uint256;
 
+  function totalSupply() public view returns (uint256) {
+    return totalSupply;
+  }
+
   function balanceOf(address _owner) public view returns (uint256) {
     return balances[_owner];
   }
@@ -31,7 +36,6 @@ contract ERC20Token is ERC20Interface {
   }
 
   function approve(address _spender, uint256 _value) public returns (bool) {
-    require(_spender != address(0));
     require((_value == 0) || (allowed[msg.sender][_spender] == 0));
 
     allowed[msg.sender][_spender] = _value;
@@ -41,8 +45,6 @@ contract ERC20Token is ERC20Interface {
   }
 
   function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-
     balances[msg.sender] = balances[msg.sender].sub(_value);
     balances[_to] = balances[_to].add(_value);
 
@@ -51,8 +53,6 @@ contract ERC20Token is ERC20Interface {
   }
 
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-
     allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
@@ -66,15 +66,26 @@ contract ERC20Token is ERC20Interface {
     totalSupply = totalSupply.sub(_value);
 
     emit Burn(msg.sender, _value);
-    emit Transfer(msg.sender, address(0), _value);
     return true;
   }
 }
 
 contract CoomiToken is ERC20Token {
-  string public constant name = 'Coomi';
-  string public constant symbol = 'COOMI';
-  uint8 public constant decimals = 18;
+  string internal constant name = 'Coomi';
+  string internal constant symbol = 'COOMI';
+  uint8 internal constant decimals = 18;
+
+  function name() public view returns (string) {
+    return name;
+  }
+
+  function symbol() public view returns (string) {
+    return symbol;
+  }
+
+    function decimals() public view returns (uint8) {
+    return decimals;
+  }
 
   constructor(uint256 _totalSupply) public {
     totalSupply = _totalSupply * 10 ** decimals;
