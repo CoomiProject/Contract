ce8f7c810864fe9782f294161d02d3a89ac7cce4
Erichin <erichin@r.recruit.co.jp>
2018-07-22 16:40:26 +0900

    update

--- CoomiToken.sol
+++ CoomiToken.sol
@@ -42,7 +42,6 @@ contract ERC20Token is ERC20Interface {
   }
 
   function approve(address _spender, uint256 _value) public returns (bool) {
-    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
     allowed[msg.sender][_spender] = _value;
     emit Approval(msg.sender, _spender, _value);
     return true;
--- CrowdSale.sol
+++ CrowdSale.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.24;
 
 library SafeMath {
-
   function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
     if (a == 0) {
       return 0;
@@ -12,23 +11,15 @@ library SafeMath {
     return c;
   }
 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
-
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
   function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
     c = a + b;
     assert(c >= a);
     return c;
   }
+}
+
+contract CoomiToken {
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
 }
 
 contract Owned {
@@ -48,27 +39,21 @@ contract Owned {
   }
 }
 
-contract CoomiToken {
-  function transferFrom(address from, address to, uint256 value) public returns (bool);
-}
-
 contract Crowdsale is Owned {
   using SafeMath for uint256;
 
   CoomiToken public coomiToken;
   uint256 public exchangeRate;
-  bool public isOpen;
   uint256 public amountRaised;
   mapping(address => uint256) public donors;
 
   constructor(CoomiToken _coomiToken, uint256 _exchangeRate) public {
     coomiToken = _coomiToken;
     exchangeRate = _exchangeRate;
-    isOpen = true;
   }
 
   function () payable public {
-    require(isOpen);
+    require(exchangeRate > 1);
     uint256 etherAmount = msg.value;
     uint256 coomiAmount = etherAmount.mul(exchangeRate);
     owner.transfer(etherAmount);
@@ -79,9 +64,5 @@ contract Crowdsale is Owned {
   
   function setExchangeRate(uint256 _exchangeRate) public onlyOwner {
     exchangeRate = _exchangeRate;
-  }
-  
-  function setIsOpen(bool _isOpen) public onlyOwner {
-    isOpen = _isOpen;
   }
 }
